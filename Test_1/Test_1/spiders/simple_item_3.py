2fc133f96a7f11630bd87dc12934634f786388d7
LinesAlex <1179662449@qq.com>
2018-10-10 21:34:44 +0800

    a4

--- Test_1/Test_1/spiders/simple_item_3.py
+++ Test_1/Test_1/spiders/simple_item_3.py
@@ -1,5 +1,6 @@
 import scrapy
 
+
 class itemSpider(scrapy.Spider):
     name = 'itemSpider'
     start_urls = ['http://lab.scrapyd.cn']
@@ -23,3 +24,7 @@ class itemSpider(scrapy.Spider):
         f.write('\n')  # 换行
         f.write('标签：' + tags)  # 写入标签
         f.close()  # 关闭文件操作
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
